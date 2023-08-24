1ef0ea80303f05753c2f7172729e29fd5159557d
qnelldo <131656049+qnelldo@users.noreply.github.com>
2023-08-24 16:03:47 +0900

    [fix] 쿠키 검색 기능 수정
    - Controller 단에서의 페이지네이션 연산을 Service 단으로 옮김
    - 무지성 GPT로 이유도 모르고 알려준 대로 돌아가니 사용했지만, 페이지네이션을 위한 총 컨텐츠 수를 체크하는 것은 검색결과의 사이즈 체크로도 되었을 듯.
    - 그래서 기존에는 쿼리가 한 번 더 발생했지만, Service 단에서 연산하면서 사이즈 체크하는 부분을 통해 쿼리를 한 번으로 줄임.

--- src/main/java/com/multi/cookies/snack/dao/SnackDAOImpl.java
+++ src/main/java/com/multi/cookies/snack/dao/SnackDAOImpl.java
@@ -20,17 +20,17 @@ public class SnackDAOImpl implements SnackDAO {
         return sqlSessionTemplate.selectList("snack.search", keyword);
     }
 
-    public int getTotalResults(String keyword) {
-        if (keyword == null) {
-            // null 키워드에 대한 처리 코드 작성
-            throw new IllegalArgumentException("키워드가 null입니다.");
-        }
-        System.out.println(keyword+" DAO");
-        return sqlSessionTemplate.selectOne("snack.getTotalResults", keyword);
-    }
-
     public SearchDTO snackInfo(int snack_id) {
         System.out.println("디테일 DAO!");
         return sqlSessionTemplate.selectOne("snack.snackInfo", snack_id);
     }
+
+    ///  public int getTotalResults(String keyword) {
+    ///      if (keyword == null) {
+    ///          // null 키워드에 대한 처리 코드 작성
+    ///          throw new IllegalArgumentException("키워드가 null입니다.");
+    ///      }
+    ///      System.out.println(keyword+" DAO");
+    ///      return sqlSessionTemplate.selectOne("snack.getTotalResults", keyword);
+    ///  }
 }
