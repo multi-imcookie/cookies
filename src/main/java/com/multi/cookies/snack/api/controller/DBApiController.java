5204565bc726fc0580abba9ac311dfe23ecfec94
ReLuee <son7162@gmail.com>
2023-08-29 09:36:30 +0900

    [feat] API 데이터 1차 걸러내기
    - 일차적으로 데이터를 걸러내도록 했습니다.
    - db에 들어가는 회사명을 수정했습니다.
    - 영양성분에 NULL 혹은 "알수없음" 값이 들어가는 항목을 제외했습니다.

--- src/main/java/com/multi/cookies/snack/api/controller/DBApiController.java
+++ src/main/java/com/multi/cookies/snack/api/controller/DBApiController.java
@@ -4,7 +4,6 @@ import com.multi.cookies.snack.api.service.DBApiService;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/multi/cookies/snack/api/service/DBApiService.java
+++ src/main/java/com/multi/cookies/snack/api/service/DBApiService.java
@@ -3,12 +3,14 @@ package com.multi.cookies.snack.api.service;
 
 import com.multi.cookies.snack.api.dto.DBApiDTO;
 import org.json.simple.parser.ParseException;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface DBApiService {
     String callHaccpAPI(int page) throws IOException;
+    @Scheduled(cron = "0 49 17 * * * *")    // cron: 초 분 시 일 월 요일 연도
     int updateDB() throws ParseException, IOException;
     int insertDB() throws IOException, ParseException;
     int initializeDB();
