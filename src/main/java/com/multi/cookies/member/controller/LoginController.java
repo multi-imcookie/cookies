e360476258dc3e735a95b6482476cd126baba14c
limwantae <apts2@naver.com>
2023-08-31 15:08:21 +0900

    [feat]최근 로그인한부분 나옴 , 소셜로그인시 마이정보보기

--- src/main/java/com/multi/cookies/member/controller/LoginController.java
+++ src/main/java/com/multi/cookies/member/controller/LoginController.java
@@ -39,10 +39,12 @@ public class LoginController {
         String kakakoApiUrl = kaKaoSocialLoginService.kakaoApiURL(request);            //카카오 소셜로그인 apiURL ,social_login.properties 파일과연관
         String findUserOrPwdUrl = "/forgotUserNameOrPassWord";                         //유저 ID or 비밀번호찾기 URL
         String signUrl = "/member/sign.jsp";                                           //회원가입 URL
+        String recentLogin=loginService.getCookie("login",request);
         model.addAttribute("naverApiUrl", naverApiUrl);
         model.addAttribute("kakakoApiUrl", kakakoApiUrl);
         model.addAttribute("findUserOrPwdUrl",findUserOrPwdUrl);
         model.addAttribute("signUrl",signUrl);
+        model.addAttribute("recentLogin",recentLogin);
         return "member/login";
     }
     //전과자 자체 로그인 controller
@@ -55,6 +57,7 @@ public class LoginController {
             return "redirect:/login?msg="+msg;
         }
         Cookie cookieId = new Cookie("id",map.get("username"));  //id pw 일치시 쿠키 생성
+        Cookie recentLogin =loginService.recentLoginCookie("login","self","/login");
         LoginDTO loginDTO = loginService.getUserInfo(map);
 //        System.out.println("loginDTO = " + loginDTO);
         session.setAttribute("memberId", loginDTO.getMember_id());
@@ -65,38 +68,28 @@ public class LoginController {
         }else{
             response.addCookie(cookieId);   //체크박스 체크시 id저장
         }
+        response.addCookie(recentLogin);
         return "redirect:/index.jsp"; //홈으로 이동
     }
     //  네이버 콜백
     @RequestMapping("/login/naver/callback")
-    public String naverCallBack(String code, String state, HttpSession session) throws Exception {
+    public String naverCallBack(String code, String state, HttpSession session,HttpServletResponse response) throws Exception {
         String accessToken = naverSocialLoginService.getToken(code, state);   //네이버 accessToken 얻기
         LoginDTO loginDTO = naverSocialLoginService.getUserInfo(accessToken); //accessToken으로 유저정보 얻기
-        boolean isTrue = loginService.hasRegisteredBefore(loginDTO);          //네이버로 로그인한적이 있는지 확인
-        if (!isTrue) {                                                        //행사용 네이버 최초 로그인 확인
-            loginService.insertUserInfo(loginDTO);
-            return "redirect:/index.jsp";
-        }
-//        System.out.println("access_token = " + access_token);
-//        System.out.println("stringBuffer = " + userInfo);
-        session.setAttribute("memberId", loginDTO.getMember_id());
-        session.setAttribute("memberNickName", loginDTO.getMember_nickname());       //세션에 로그인 고유값 넣기 ?! 뭘넣지?
+        RegisterAndSession(loginDTO,session); //DB에 유저정보 저장 , session에 id값 저장
+        Cookie recentLogin =loginService.recentLoginCookie("login","naver","/login");
+        response.addCookie(recentLogin);
         return "redirect:/index.jsp";
     }
     //  카카오 콜백
     @RequestMapping("/login/kakao/callback")
-    public String kakaoCallBack(String code, HttpSession session) throws Exception {
+    public String kakaoCallBack(String code, HttpSession session,HttpServletResponse response) throws Exception {
 //        System.out.println("code" + code); //콜백 코드 출력
         String access_Token = kaKaoSocialLoginService.getToken(code);           //네이버 토큰값 얻기
         LoginDTO loginDTO = kaKaoSocialLoginService.getUserInfo(access_Token);  //토큰 값으로 유저 정보 얻기
-//        System.out.println("userInfo " + loginDTO);  //dto 출력
-        boolean isTrue = loginService.hasRegisteredBefore(loginDTO);            //이전에 카카오로 로그인했었는지 확인
-        if (!isTrue) {                                                          //언젠간 사용할  - 최초로그인시...
-            loginService.insertUserInfo(loginDTO);
-            return "redirect:/index.jsp";
-        }
-        session.setAttribute("memberId", loginDTO.getMember_id());
-        session.setAttribute("memberNickName", loginDTO.getMember_nickname());
+        RegisterAndSession(loginDTO,session); //DB에 저장
+        Cookie recentLogin =loginService.recentLoginCookie("login","kakao","/login");
+        response.addCookie(recentLogin);
         return "redirect:/index.jsp";
     }
     // 아이디 비밀번호 찾기
@@ -112,5 +105,14 @@ public class LoginController {
         //네이버 로그아웃에 대한 별도의 api가 없으며 사용자가 직접 네이버 서비스에서 로그아웃 하도록 처리하셔야 합니다.
         //이유는 이용자 보호를 위해 정책상 네이버 이외의 서비스에서 네이버 로그아웃을 수행하는 것을 허용하지 않고 있는 점 양해 부탁드립니다.
         return "redirect:/index.jsp";
+    }
+    private void RegisterAndSession(LoginDTO loginDTO,HttpSession session){
+        boolean isTrue = loginService.hasRegisteredBefore(loginDTO);            //이전에 로그인한적이있는지 검사
+        if (!isTrue) {                                                          //이전에 로그인한적없으면
+            loginService.insertUserInfo(loginDTO);                              //DB에 로그인 정보 저장
+        }
+        LoginDTO DBloginDTO= loginService.getLoginDTOByKey(loginDTO);           //DB DTO 를 가져옴
+        session.setAttribute("memberId", DBloginDTO.getMember_id());        //세션에 memberId 값 지정
+        session.setAttribute("memberNickName", DBloginDTO.getMember_nickname());
     }
 }
--- src/main/java/com/multi/cookies/member/service/SignService.java
+++ src/main/java/com/multi/cookies/member/service/SignService.java
@@ -3,16 +3,20 @@ package com.multi.cookies.member.service;
 import com.multi.cookies.member.dao.SignDAO;
 import com.multi.cookies.member.dto.MemberDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignService {
     @Autowired
     SignDAO signDAO;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public int sign(MemberDTO memberDTO){
-        signDAO.sign(memberDTO);
-        return 0;
+        String encodePw = bCryptPasswordEncoder.encode(memberDTO.getMember_pw());
+        memberDTO.setMember_pw(encodePw);
+        return signDAO.sign(memberDTO);
     }
 
     public int checkId(String member_signId) {
