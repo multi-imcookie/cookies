2dcbe49ba8f5881f1fceb8386e08bed3a760c4d3
ReLuee <son7162@gmail.com>
2023-09-01 16:22:30 +0900

    [feat] API 기능 고도화 및 스케쥴러 추가
    - DB에 들어오는 데이터가 좀 더 완전해졌습니다.
    - 초콜릿 정보도 받아오도록 수정했습니다.
    - 자동 업데이트 스케쥴러를 추가했습니다. (기본설정: 오전 5시)

---
+++ src/main/java/com/multi/cookies/snack/api/scheduler/Scheduler.java
@@ -0,0 +1,22 @@
+package com.multi.cookies.snack.api.scheduler;
+
+import com.multi.cookies.snack.api.service.DBApiService;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class Scheduler {
+
+    @Autowired
+    DBApiService dbApiService;
+
+    @Scheduled(cron = "0 0 5 * * *")   // 초 분 시 일 월 요일
+    public void autoUpdate() throws ParseException, IOException {
+        dbApiService.updateDB();
+        System.out.println("자동 업데이트 완료");
+    }
+}
--- src/main/java/com/multi/cookies/snack/api/service/DBApiService.java
+++ src/main/java/com/multi/cookies/snack/api/service/DBApiService.java
@@ -9,10 +9,9 @@ import java.io.IOException;
 import java.util.List;
 
 public interface DBApiService {
-    String callHaccpAPI(int page) throws IOException;
-    @Scheduled(cron = "0 49 17 * * * *")    // cron: 초 분 시 일 월 요일 연도
+    String callHaccpAPI(int page, String prdkind) throws IOException;
     int updateDB() throws ParseException, IOException;
 //    int insertDB() throws IOException, ParseException;
     int initializeDB();
-    List<DBApiDTO> parseJsonData(int page) throws ParseException, IOException;
+    List<DBApiDTO> parseJsonData(int page, String prdkind) throws ParseException, IOException;
 }
