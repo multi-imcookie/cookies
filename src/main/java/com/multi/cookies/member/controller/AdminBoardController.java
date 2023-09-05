4b5ef213e8fec6a286ff9209661ee3be6d5312f3
Lee Sungbo <128334653+ExiledI@users.noreply.github.com>
2023-09-05 10:04:38 +0900

    [fix]자유게시판 데이터 안나오는 버그 수정
    버그 수정

--- src/main/java/com/multi/cookies/member/controller/AdminBoardController.java
+++ src/main/java/com/multi/cookies/member/controller/AdminBoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 @Controller
 public class AdminBoardController {
     @Autowired
@@ -16,24 +17,29 @@ public class AdminBoardController {
                              @RequestParam(required = false) String searchType,
                              @RequestParam(required = false) String keyword,
                              Model model) {
-        model.addAttribute("boardList", adminBoardService.getBoards(searchType, keyword, page));
+        model.addAttribute("boardsWithPaging", adminBoardService.getBoards(searchType, keyword, page));
         return "member/boards";
     }
-    @GetMapping("/{id}")
-    public AdminBoardDTO getBoardById(@PathVariable int id) {
-        return adminBoardService.getBoardById(id);
+
+    @GetMapping("/board/{id}")
+    public String getBoardById(@PathVariable int id, Model model) {
+        model.addAttribute("board", adminBoardService.getBoardById(id));
+        return "member/viewboard";
     }
+
     @GetMapping("/update/{id}")
     public String updateForm(@PathVariable int id, Model model) {
         AdminBoardDTO board = adminBoardService.getBoardById(id);
         model.addAttribute("board", board);
         return "member/editBoard";
     }
+
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable int id) {
         adminBoardService.deleteBoard(id);
-        return "redirect:member/boards";
+        return "redirect:/boards";
     }
+
     @GetMapping("/view/{id}")
     public String view(@PathVariable int id, Model model) {
         AdminBoardDTO board = adminBoardService.getBoardById(id);
@@ -41,3 +47,4 @@ public class AdminBoardController {
         return "member/viewboard";
     }
 }
+
